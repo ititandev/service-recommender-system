90ea3501cf88bb9f251c46bbca233c76d97c6cd0
ititan <ititandev>
2019-05-10 11:52:47 +0700

    add jwt authentication

--- server/app.js
+++ server/app.js
@@ -21,8 +21,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/get_name',nameRouter);
+app.use('/', usersRouter);
+app.use('/',nameRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ server/auth.js
@@ -0,0 +1,46 @@
+const jwt = require('jsonwebtoken');
+const _ = require("lodash");
+
+function verifyJWTToken(token) {
+    return new Promise((resolve, reject) => {
+        jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken) => {
+            if (err || !decodedToken) {
+                return reject(err)
+            }
+
+            resolve(decodedToken)
+        })
+    })
+}
+
+
+function createJWToken(details) {
+    if (typeof details !== 'object') {
+        details = {}
+    }
+
+    if (!details.maxAge || typeof details.maxAge !== 'number') {
+        details.maxAge = 3600
+    }
+
+    details.sessionData = _.reduce(details.sessionData || {}, (memo, val, key) => {
+        if (typeof val !== "function" && key !== "password") {
+            memo[key] = val
+        }
+        return memo
+    }, {})
+
+    let token = jwt.sign({
+        data: details.sessionData
+    }, process.env.JWT_SECRET, {
+            expiresIn: details.maxAge,
+            algorithm: 'HS256'
+        })
+
+    return token
+}
+
+module.exports = {
+    verifyJWTToken,
+    createJWToken
+}
