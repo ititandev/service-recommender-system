d396243bc72479f84f8c00105cbb9aa59a0286b3
ititan <ititandev>
2019-05-11 08:48:41 +0700

    refactor user routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,52 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
+const UserModel = require('../schema/ServiceModel');
+const { createJWToken, verifyJWTToken } = require('../auth.js');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+mongoose.connect('mongodb://servicy:servicy123@ds151416.mlab.com:51416/servicy', { useNewUrlParser: true });
+router.get('/users', function (req, res, next) {
+  UserModel.find((err, data) => {
+    if (err) {
+      res.json({
+        status: "error",
+        message: "Some error happen"
+      });
+    }
+    else
+      res.json({
+        status: "success",
+        data: data
+      })
+  })
+
 });
+
+router.get('/login', function (req, res, next) {
+  res.status(200)
+      .json({
+          success: true,
+          token: createJWToken({
+              sessionData: 1,
+              maxAge: 604800
+          })
+      })
+
+})
+
+router.get('/signup', function (req, res, next) {
+  
+
+})
+
+router.get('/checktoken/:token', function (req, res, next) {
+  verifyJWTToken(req.param("token")).then(
+      (any) => {
+          res.send("dung cmnr")
+      },
+      (err) => {
+          res.send("nhu cc")
+      })
+})
 
 module.exports = router;
