d8e4c7d12a31259ba82da0e66bc73ba1a7c67e72
caochanhduong <caochanhduongbku@gmail.com>
2019-05-11 11:25:17 +0700

    fix service & locations & servicetypes  api

--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var servicesRouter = require('./routes/services');
 var servicetypesRouter = require('./routes/servicetypes');
-
+var locationRouter=require('./routes/locations');
 var app = express();
 
 // view engine setup
@@ -25,7 +25,7 @@ app.use('/', indexRouter);
 app.use('/', usersRouter);
 app.use('/', servicesRouter);
 app.use('/', servicetypesRouter);
-
+app.use('/', locationRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- server/routes/services.js
+++ server/routes/services.js
@@ -4,9 +4,17 @@ const mongoose = require('mongoose');
 const {matchName} = require('./utils');
 const ServiceModel = require('../schema/ServiceModel')
 const ServiceTypeModel =require('../schema/ServiceTypeModel');
-
+const LocationModel=require('../schema/LocationModel');
 mongoose.connect('mongodb://servicy:servicy123@ds151416.mlab.com:51416/servicy', {useNewUrlParser: true});
 
+router.get('/services/best', function(req, res, next) {
+    ServiceModel.find({},{rating: 1, avatar: 1, description: 1, name: 1}).then(services => {
+        services = services.sort((a, b) => a.rating.points/a.rating.total < b.rating.points/b.rating.total).slice(0, 3);
+        res.json({success: true,message:"Find best services",data:services});
+    }).catch(err => {
+        res.json({success:false,message:"error"});
+    });
+});
 router.get('/services', function(req, res , next) {
     const {locationName, serviceType, filterText} = req.query;
     ServiceModel.find({})
@@ -47,7 +55,8 @@ router.get('/services', function(req, res , next) {
 });
 
 router.get('/services/:id', function(req, res , next) {
-    const {serviceId} = req.param("id");
+    const serviceId = req.param("id");
+    console.log(serviceId);
     ServiceModel.find({})
     .populate('category_id')
     .populate('info.location_id')
@@ -57,21 +66,8 @@ router.get('/services/:id', function(req, res , next) {
         } else {
             let result = []
             for (let service of docs) {
-                let filterCondition = matchName(service.description, filterText) || 
-                matchName(service.info.address,filterText) ||
-                matchName(service.info.price,filterText) ||
-                matchName(service.info.website,filterText) ||
-                matchName(service.info.content,filterText);
-
-                if (service.info.location_id !== null && locationName!==undefined) {
-                    filterCondition = filterCondition && matchName(service.info.location_id.name,locationName) 
-                }
-
-                if (service.category_id !== null && serviceType!==undefined){
-                    filterCondition = filterCondition && matchName(service.category_id.name,serviceType) 
-                }
-
-                if (filterCondition) {
+                if (service._id==serviceId)
+                {
                     result.push(service);
                 }
             }
@@ -79,19 +75,12 @@ router.get('/services/:id', function(req, res , next) {
             if (result.length < 1) {
                 res.json({success:false,data:result,message:"Result not found"})
             } else {
-                res.json({success: true,data: result,message:"Found result"});
+                res.json({success: true,data: result,message:"Found service"});
             }
         }
     });
 });
 
 
-router.get('/services/best', function(req, res, next) {
-    ServiceModel.find({},{rating: 1, avatar: 1, description: 1, name: 1}).then(services => {
-        services = services.sort((a, b) => a.rating.points/a.rating.total < b.rating.points/b.rating.total).slice(0, 3);
-        res.json({success: true,message:"Find best services",data:services});
-    }).catch(err => {
-        res.json({success:false,message:"error"});
-    });
-});
+
 module.exports = router;
