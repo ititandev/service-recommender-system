4797af7dbba6ad7b64230b6d9ec2dcb37149cb7a
ititandev <ldmanhit@gmail.com>
2019-05-11 21:55:12 +0700

    change Origin header to *

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'jade');
 
 app.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://servicy.herokuapp.com/');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   // Request headers you wish to allow
