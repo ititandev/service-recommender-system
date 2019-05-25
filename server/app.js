daa09f63f054731ad89f84c9ba0dc1e31a3042de
ititandev <ldmanhit@gmail.com>
2019-05-25 20:25:33 +0700

    add homepage from Kiet

--- server/app.js
+++ server/app.js
@@ -36,7 +36,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/', indexRouter);
+app.use('/', indexRouter);
 app.use('/api/', usersRouter);
 app.use('/api/', servicesRouter);
 app.use('/api/', adsRouter);
