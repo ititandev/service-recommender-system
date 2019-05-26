ea60dc54e4aeeb1ac9fb6e53b99859ad478ef28e
anh2003maa <thanductaimgt@gmail.com>
2019-05-26 19:57:54 +0700

    change ads alert

--- UI/material-dashboard-react/src/views/Advertisements/Advertisements.jsx
+++ UI/material-dashboard-react/src/views/Advertisements/Advertisements.jsx
@@ -138,8 +138,8 @@ class Advertisements extends React.Component {
           </Card>
         </GridItem>
         {this.state.openDeleteDialog ? <AlertDialog
-          title={"Xác nhận xoá loại dịch vụ?"}
-          description={"Bạn đang thực hiện xóa loại dịch vụ được chọn khỏi hệ thống. Thao tác này không thể được hoàn tác, hãy xác nhận rằng bạn chắc chắn muốn thực hiện thay đổi này."}
+          title={"Xác nhận xoá quảng cáo?"}
+          description={"Bạn đang thực hiện xóa quảng cáo được chọn khỏi hệ thống. Thao tác này không thể được hoàn tác, hãy xác nhận rằng bạn chắc chắn muốn thực hiện thay đổi này."}
           handleCancel={() => {
             this.setState({
               ...this.state,
