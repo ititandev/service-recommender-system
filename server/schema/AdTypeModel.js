eade2560bd144a32c0f68ee2e0fd27cacdefb1cf
ititandev <ldmanhit@gmail.com>
2019-05-28 21:10:33 +0700

    delete redundant files

--- server/schema/AdTypeModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const AdTypeSchema = new Schema({
-    name: String,
-    max_views: Number
-});
-const AdTypeModel = mongoose.model('adtypes', AdTypeSchema);
-
-module.exports = AdTypeModel
