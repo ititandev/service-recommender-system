4171935f6adb395ece53fce21e661bedc14a9cc7
tadevi <1611695@hcmut.edu.vn>
2019-05-31 16:53:43 +0700

    fix bugs deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-#params: admin, homepage, provider
+#!/bin/bash
 declare -A args
 args=(["admin"]=1 ["home"]=1 ["provider"]=1)
 name="all"
@@ -24,7 +24,7 @@ then
 	name=$param
 	deploy
 else
-	echo "using command: ./deploy.sh admin|provider|homepage|all"
+	echo "using command: ./deploy.sh admin|provider|home|all"
 fi
 
 
